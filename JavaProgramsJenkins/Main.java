65e63184886981bd9083ed174dbf04920ca25cf5
Joel Oliveira <joel80851@gmail.com>
2024-08-07 11:52:56 +0530

    --

--- JavaProgramsJenkins/Main.java
+++ JavaProgramsJenkins/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is a java program");
         System.out.println(new Date());
-        System.out.println("Hello");
+//        System.out.println("Hello");
+        Student s1 = new Student();
+        s1.sayHello();
     }
 }
---
+++ JavaProgramsJenkins/Student.java
@@ -0,0 +1,5 @@
+public class Student {
+    public String sayHello() {
+        return  ("Hey, I am a Student !");
+    }
+}
