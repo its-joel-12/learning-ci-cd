d918023e7b8fa97e85e8d13409d4698c90af8947
Joel Oliveira <joel80851@gmail.com>
2024-08-07 11:31:13 +0530

    Update Main.java

--- JavaProgramsJenkins/Main.java
+++ JavaProgramsJenkins/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is a java program");
         System.out.println(new Date());
+        System.out.println("Hello");
     }
 }
